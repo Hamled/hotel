869842280eeddee5425a24d85b4dd64382bd3742
Charles Ellis <hamled@hamled.name>
2017-09-15 00:18:20 -0700

    DateRange only works with Date objects

--- lib/hotel_manager/date_range.rb
+++ lib/hotel_manager/date_range.rb
@@ -1,4 +1,11 @@
 require 'date'
 
 class HotelManager::DateRange < Range
+  def initialize(begin_date, end_date)
+    unless begin_date.is_a?(Date) && end_date.is_a?(Date)
+      raise ArgumentError.new("DateRange.new must be passed two Date objects")
+    end
+
+    super(begin_date, end_date)
+  end
 end
--- specs/date_range_spec.rb
+++ specs/date_range_spec.rb
@@ -16,5 +16,13 @@ describe DateRange do
     it "stores an end date" do
       @range.end.must_equal @end
     end
+
+    it "raises ArgumentError if not given two dates" do
+      proc{ DateRange.new(@begin, nil) }.must_raise ArgumentError
+      proc{ DateRange.new(nil, @end) }.must_raise ArgumentError
+      proc{ DateRange.new(@begin, 5) }.must_raise ArgumentError
+      proc{ DateRange.new(1, @end) }.must_raise ArgumentError
+      proc{ DateRange.new(1, 5) }.must_raise ArgumentError
+    end
   end
 end
